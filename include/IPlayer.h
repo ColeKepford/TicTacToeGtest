635cbd3748e3078b40be414de6b00fb3e611c7ed
kepc2729 <kepc2729@mailhost.cs.uleth.ca>
2022-11-17 15:06:15 +0000

    added variables and method

--- include/IPlayer.h
+++ include/IPlayer.h
@@ -2,6 +2,11 @@
 #define INCLUDE_IPLAYER_H_
 
 class IPlayer {
-    // placeholder class
+    public:
+        char letter;
+        int playerNum;
+        enum type;
+
+        virtual void clickTile(int x, int y, char letter) = 0;
 };
 #endif  // INCLUDE_IPLAYER_H_
