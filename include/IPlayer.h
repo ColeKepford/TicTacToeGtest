6633cfcc0ac6e1002f55204c84ebc67e16fc5ff5
kepc2729 <kepc2729@mailhost.cs.uleth.ca>
2022-11-17 15:10:13 +0000

    Update include/IPlayer.h

--- include/IPlayer.h
+++ include/IPlayer.h
@@ -1,3 +1,13 @@
+/**
+ * @file IBoardGame.h
+ * @author Cole Kepford (cole.kepford@uleth.ca)
+ * @brief Interface for a 2D board game class.
+ * @version 0.1
+ * @date 2022-11-17
+ *
+ * @copyright Copyright (c) 2022
+ *
+ */
 #ifndef INCLUDE_IPLAYER_H_
 #define INCLUDE_IPLAYER_H_
 
