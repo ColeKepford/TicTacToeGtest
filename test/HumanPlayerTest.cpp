2e66f25a55d25e66d8b65db85e4fecca3885885d
Adam Schlinker <adam.schlinker@uleth.ca>
2022-11-23 15:51:58 -0700

    Changed all '.' to '->' in HPTests.cpp

--- test/HumanPlayerTest.cpp
+++ test/HumanPlayerTest.cpp
@@ -22,60 +22,45 @@ class PlayerTests :public::testing::Test {
 
 TEST_F(PlayerTests, testAssignLetterX) {
     player.setLetter('X');
-    EXPECT_THAT(validLetters, Contains(player.getLetter()));
+    EXPECT_THAT(validLetters, Contains(player->getLetter()));
 }
 
 TEST_F(PlayerTests, testAssignLetterO) {
     player.setLetter('O');
-    EXPECT_THAT(validLetters, Contains(player.getLetter()));
+    EXPECT_THAT(validLetters, Contains(player->getLetter()));
 }
 
 TEST_F(PlayerTests, testAssignLetterInvalid) {
     player.setLetter('A');
-    EXPECT_THAT(validLetters, Contains(player.getLetter()));
+    EXPECT_THAT(validLetters, Contains(player->getLetter()));
 }
 
 TEST_F(PlayerTests, testAssignLetterNULL) {
     player.setLetter(NULL);
-    EXPECT_THAT(validLetters, Contains(player.getLetter()));
+    EXPECT_THAT(validLetters, Contains(player->getLetter()));
 }
 
 TEST_F(PlayerTests, testAssignPlayerNum1) {
     player.setPlayerNum(1);
-    EXPECT_THAT(validPlayerNum, Contains(player.getPlayerNum()));
+    EXPECT_THAT(validPlayerNum, Contains(player->getPlayerNum()));
 }
 
 TEST_F(PlayerTests, testAssignPlayerNum2) {
     player.setPlayerNum(2);
-    EXPECT_THAT(validPlayerNum, Contains(player.getPlayerNum()));
+    EXPECT_THAT(validPlayerNum, Contains(player->getPlayerNum()));
 }
 
 TEST_F(PlayerTests, testAssignPlayerNumInvalid) {
     player.setPlayerNum(42);
-    EXPECT_THAT(validPlayerNum, Contains(player.getPlayerNum()));
+    EXPECT_THAT(validPlayerNum, Contains(player->getPlayerNum()));
 }
 
 TEST_F(PlayerTests, testAssignPlayerNumNULL) {
     player.setPlayerNum(NULL);
-    EXPECT_THAT(validPlayerNum, Contains(player.getPlayerNum()));
+    EXPECT_THAT(validPlayerNum, Contains(player->getPlayerNum()));
 }
 
 TEST_F(PlayerTests, testAssignPlayerTypeHuman) {
     player.setType("Human");
-    EXPECT_THAT(validType, Contains(player.getType()));
-}
-
-TEST_F(PlayerTests, testAssignPlayerTypeComputer) {
-    player.setType("Computer");
-    EXPECT_THAT(validType, Contains(player.getType()));
-}
-
-TEST_F(PlayerTests, testAssignPlayerTypeInvalid) {
-    player.setType("Fish");
-    EXPECT_THAT(validType, Contains(player.getType()));
-}
-
-TEST_F(PlayerTests, testAssignPlayerTypeNULL) {
-    player.setType(NULL);
-    EXPECT_THAT(validType, Contains(player.getType()));
+    EXPECT_THAT(validType, Contains(player->getType()));
 }
