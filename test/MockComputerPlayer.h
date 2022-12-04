f33e8773bf5791f8270d885ed7789ba1f0e44118
kepc2729 <kepc2729@mailhost.cs.uleth.ca>
2022-12-04 00:59:37 +0000

    Add new file

---
+++ test/MockComputerPlayer.h
@@ -0,0 +1,35 @@
+#pragma once
+#include"../include/IPlayer.h"
+#include"../include/IBoardGame.h"
+class MockComputerPlayer : public IPlayer {
+    public:
+        MockComputerPlayer() {
+        IPlayer:letter = 'O';
+        IPLayer:playerNum = 2;
+        IPlayer:type = "computer";
+        }
+
+        void clickTile(int x, int y, char letter, IBoardGame* game) {
+
+        }
+
+        char getLetter() {
+
+         }
+
+        int getPlayerNum() {
+
+        }
+
+        std::string getType() {
+
+        }
+
+        void setLetter(char letter) {
+
+        }
+
+        void setPlayerNum() {
+
+        }
+};
