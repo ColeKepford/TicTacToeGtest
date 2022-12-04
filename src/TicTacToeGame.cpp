ad1a5efa5878578b505378b8526af38645aceefd
Cole Kepford <cole.kepford@uleth.ca>
2022-12-04 14:19:12 -0700

    All tests passing. Fixed TicTacToeGame errors, changed Players to pair rather than vector

--- src/TicTacToeGame.cpp
+++ src/TicTacToeGame.cpp
@@ -1,10 +1,154 @@
 #include "TicTacToeGame.h"
 #include "IPlayer.h"
+#include <stdexcept>
 
+
+TicTacToeGame::TicTacToeGame() {
+    turn = 1;
+    players.first = nullptr;
+    players.second = nullptr;
+    createBoard(3);
+    currentPlayerNum = 1;
+}
+
+TicTacToeGame::~TicTacToeGame() {
+    if (players.first != nullptr) {
+        delete players.first;
+    }
+    if (players.second != nullptr) {
+        delete players.second;
+    }
+}
+
+TicTacToeGame::TicTacToeGame(std::pair<IPlayer*, IPlayer*> i_players) {
+    turn = 0;
+    players = i_players;
+    currentPlayerNum = i_players.first->getPlayerNum();
+    createBoard(3);
+}
+
+
+
+TicTacToeGame::TicTacToeGame(int size, std::pair<IPlayer*, IPlayer*> i_players) {
+    turn = 0;
+    players = i_players;
+    currentPlayerNum = players.first->getPlayerNum();
+    createBoard(size);
+}
+//need to readjust this
 void TicTacToeGame::assignPlayer(IPlayer* player) {
+    if (player == nullptr) {
+        throw std::invalid_argument("Null player!");
+    }
+    else if(players.first == nullptr) {
+        players.first = player;
+    }
+    else if(players.second == nullptr) {
+        players.second = player;
+    }
+}
+
+bool TicTacToeGame::modifyTile(int x, int y, char letter) {
+    if (letter != 'X' && letter != 'O' && letter != 'N') {
+        throw std::invalid_argument("Invalid character.");
+        return false;
+    }
+    else if (x < 0 || x > 2 || y < 0 || y > 2) {
+        throw std::invalid_argument("Invalid coordinate.");
+        return false;
+    }
+    else if (this->board[x][y] == 'N') {
+        this->board[x][y] = letter;
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+int TicTacToeGame::checkEnd() {
+
+    //check diagonals
+    if ((board[0][0] == 'O' && board[1][1] == 'O' && board[2][2] == 'O')
+        || (board[0][2] == 'O' && board[1][1] == 'O' && board[2][0] == 'O')) {
+        return 1;
+    }
+    else if ((board[0][0] == 'X' && board[1][1] == 'X' && board[2][2] == 'X')
+        || (board[0][2] == 'X' && board[1][1] == 'X' && board[2][0] == 'X')) {
+        return 2;
+    }
+
+    //check rows ands cols
+    for (int i = 0; i < board.size(); i++) {
+        if ((board[0][i] == 'O' && board[1][i] == 'O' && board[2][i] == 'O')
+            || (board[i][0] == 'O' && board[i][1] == 'O' && board[i][2] == 'O')) {
+            return 1;
+        }
+        else if ((board[0][i] == 'X' && board[1][i] == 'X' && board[2][i] == 'X')
+            || (board[i][0] == 'X' && board[i][1] == 'X' && board[i][2] == 'X')) {
+            return 2;
+        }
+    }
+    if (turn > 8) {
+        return 0;
+    }
+
+    //return -1 and keep the game going
+    return -1;
 
 }
 
+void TicTacToeGame::createBoard(int size) {
+    if (size != 3) {
+        throw std::invalid_argument("Invalid size.");
+    }
+    std::vector<std::vector<char> > board(
+        size,
+        std::vector<char>(size, 'N'));
+    
+    this->board = board;
+}
+
+void TicTacToeGame::endTurn() {
+    this->turn += 1;
+
+    if (currentPlayerNum == players.first->getPlayerNum()) {
+        currentPlayerNum = players.second->getPlayerNum();
+    }
+    else {
+        currentPlayerNum = players.first->getPlayerNum();
+    }
+}
+
+int TicTacToeGame::getBoardSize() {
+    int size = 0;
+
+    for (int i = 0; i < board.size(); i++) {
+        size += board[i].size();
+    }
+    return size;
+}
+
 int TicTacToeGame::getCurrentPlayerNum() {
-    return 0;
+    return currentPlayerNum;
+}
+
+int TicTacToeGame::getTurn() {
+    return turn;
+}
+
+std::vector<std::vector<char>> TicTacToeGame::getBoard() {
+    return board;
+}
+
+std::pair<IPlayer*, IPlayer*> TicTacToeGame::getPlayers() {
+    return players;
+}
+
+void TicTacToeGame::setBoard(std::vector<std::vector<char>> board) {
+    this->board = board;
+}
+
+void TicTacToeGame::setPlayers(std::pair<IPlayer*, IPlayer*> i_players) {
+    this->players = players;
 }
--- test/TicTacToeGameTests.cpp
+++ test/TicTacToeGameTests.cpp
@@ -1,4 +1,4 @@
-/*#include "pch.h"
+#include "pch.h"
 #include "../include/TicTacToeGame.h"
 #include"MockHumanPlayer.h"
 #include"MockComputerPlayer.h"
@@ -23,7 +23,6 @@ TEST_F(GameTests, testAssignPlayerNotNull) {
     try {
         MockHumanPlayer* human2 = new MockHumanPlayer();
         game.assignPlayer(human2);
-        delete human2;
     }
     catch (std::invalid_argument& e) {
         std::cerr << e.what() << std::endl;
@@ -37,44 +36,68 @@ TEST_F(GameTests, testAssignPlayerNull) {
         game.assignPlayer(human2);
     }
     catch (std::invalid_argument& e) {
-        EXPECT_EQ("Player is null.", e.what());
+        std::string message = e.what();
+        EXPECT_EQ("Null player!", message);
     }
 }
 
 TEST_F(GameTests, testCheckEndNotOver) {
     EXPECT_EQ(-1, game.checkEnd());
 }
+TEST_F(GameTests, testCheckEndOverWithXsBottomRow) {
+    game.modifyTile(2, 0, 'X');
+    game.modifyTile(2, 1, 'X');
+    game.modifyTile(2, 2, 'X');
 
-TEST_F(GameTests, testCheckEndOverWithXs) {
-    game.getBoard()[0][1] = 'X';
-    game.getBoard()[1][1] = 'X';
-    game.getBoard()[2][1] = 'X';
+    EXPECT_EQ(2, game.checkEnd());
+}
+
+TEST_F(GameTests, testCheckEndOverWithOsBottomRow) {
+    game.modifyTile(2, 0, 'O');
+    game.modifyTile(2, 1, 'O');
+    game.modifyTile(2, 2, 'O');
+
+    EXPECT_EQ(1, game.checkEnd());
+}
+TEST_F(GameTests, testCheckEndOverWithXsMiddleColumn) {
+    game.modifyTile(0, 1, 'X');
+    game.modifyTile(1, 1, 'X');
+    game.modifyTile(2, 1, 'X');
+
+    EXPECT_EQ(2, game.checkEnd());
+}
+
+TEST_F(GameTests, testCheckEndOverWithOsMiddleColumn) {
+    game.modifyTile(0, 1, 'O');
+    game.modifyTile(1, 1, 'O');
+    game.modifyTile(2, 1, 'O');
 
     EXPECT_EQ(1, game.checkEnd());
 }
 
-TEST_F(GameTests, testCheckEndOverWithOs) {
-    game.getBoard()[0][1] = 'O';
-    game.getBoard()[1][1] = 'O';
-    game.getBoard()[2][1] = 'O';
+TEST_F(GameTests, testCheckEndOverWithOsDiagonalRight) {
+    game.modifyTile(0, 0, 'O');
+    game.modifyTile(1, 1, 'O');
+    game.modifyTile(2, 2, 'O');
 
     EXPECT_EQ(1, game.checkEnd());
 }
 
-TEST_F(GameTests, testCheckEndOverWithDraw) {
-    //Column 1
-    game.getBoard()[0][0] = 'O';
-    game.getBoard()[1][0] = 'O';
-    game.getBoard()[2][0] = 'X';
-    //Column2
-    game.getBoard()[0][1] = 'X';
-    game.getBoard()[1][1] = 'X';
-    game.getBoard()[2][1] = 'O';
-    //Column3
-    game.getBoard()[0][2] = 'O';
-    game.getBoard()[1][2] = 'X';
-    game.getBoard()[2][2] = 'X';
+TEST_F(GameTests, testCheckEndOverWithXsDiagonalRight) {
+    game.modifyTile(0, 0, 'X');
+    game.modifyTile(1, 1, 'X');
+    game.modifyTile(2, 2, 'X');
 
+    EXPECT_EQ(2, game.checkEnd());
+}
+
+TEST_F(GameTests, testCheckEndOverWithDrawFullBoard) {
+    game.assignPlayer(new MockHumanPlayer());
+    game.assignPlayer(new MockComputerPlayer());
+    for (int i = 0; i < 9; i++) {
+        game.endTurn();
+    }
+    
     EXPECT_EQ(0, game.checkEnd());
 }
 
@@ -83,8 +106,8 @@ TEST_F(GameTests, TestCreateBoardSize3) {
     std::vector<std::vector<char>> board = game.getBoard();
     EXPECT_EQ(9, game.getBoardSize());
     char letter;
-    for (int i = 0; i < game.getBoardSize(); i++) {
-        for (int j = 0; j < game.getBoardSize(); j++) {
+    for (int i = 0; i < board.size(); i++) {
+        for (int j = 0; j < board[i].size(); j++) {
             letter = board[i][j];
             EXPECT_EQ('N', letter);
         }
@@ -96,7 +119,8 @@ TEST_F(GameTests, TestCreateBoardSize2) {
         game.createBoard(2);
     }
     catch (std::invalid_argument& e) {
-        EXPECT_EQ("Invalid size.", e.what());
+        std::string message = e.what();
+        EXPECT_EQ("Invalid size.", message);
     }
 }
 
@@ -105,7 +129,8 @@ TEST_F(GameTests, TestCreateBoardSize100) {
         game.createBoard(100);
     }
     catch (std::invalid_argument& e) {
-        EXPECT_EQ("Invalid size.", e.what());
+        std::string message = e.what();
+        EXPECT_EQ("Invalid size.", message);
     }
 }
 
@@ -114,7 +139,8 @@ TEST_F(GameTests, TestCreateBoardSizeZero) {
         game.createBoard(0);
     }
     catch (std::invalid_argument& e) {
-        EXPECT_EQ("Invalid size.", e.what());
+        std::string message = e.what();
+        EXPECT_EQ("Invalid size.", message);
     }
 }
 
@@ -123,7 +149,8 @@ TEST_F(GameTests, TestCreateBoardSizeNegative) {
         game.createBoard(-1);
     }
     catch (std::invalid_argument& e) {
-        EXPECT_EQ("Invalid size.", e.what());
+        std::string message = e.what();
+        EXPECT_EQ("Invalid size.", message);
     }
 }
 
@@ -138,8 +165,6 @@ TEST_F(GameTests, TestEndTurn1) {
     EXPECT_NE(turn, game.getTurn());
     EXPECT_NE(playerNum, game.getCurrentPlayerNum());
     EXPECT_EQ(2, game.getTurn());
-    delete computer2;
-    delete human2;
 }
 
 TEST_F(GameTests, TestEndTurn2) {
@@ -154,8 +179,6 @@ TEST_F(GameTests, TestEndTurn2) {
     EXPECT_NE(turn, game.getTurn());
     EXPECT_EQ(playerNum, game.getCurrentPlayerNum());
     EXPECT_EQ(3, game.getTurn());
-    delete computer2;
-    delete human2;
 }
 
 TEST_F(GameTests, TestEndTurn3) {
@@ -188,7 +211,7 @@ TEST_F(GameTests, TestModifyTileValid) {
 TEST_F(GameTests, TestModifyTileValid2) {
     try {
         game.modifyTile(2, 2, 'O');
-        char O = game.getBoard()[1][1];
+        char O = game.getBoard()[2][2];
         EXPECT_EQ('O', O);
     }
     catch (std::invalid_argument& e) {
@@ -201,7 +224,8 @@ TEST_F(GameTests, TestModifyTileNegativeX) {
         game.modifyTile(-1, 2, 'O');
     }
     catch (std::invalid_argument& e) {
-        EXPECT_EQ("Invalid coordinate.", e.what());
+        std::string message = e.what();
+        EXPECT_EQ("Invalid coordinate.", message);
     }
 }
 
@@ -210,7 +234,8 @@ TEST_F(GameTests, TestModifyTileNegativeY) {
         game.modifyTile(2, -1, 'O');
     }
     catch (std::invalid_argument& e) {
-        EXPECT_EQ("Invalid coordinate.", e.what());
+        std::string message = e.what();
+        EXPECT_EQ("Invalid coordinate.", message);
     }
 }
 
@@ -219,7 +244,8 @@ TEST_F(GameTests, TestModifyTileTooLargeX) {
         game.modifyTile(5, 2, 'O');
     }
     catch (std::invalid_argument& e) {
-        EXPECT_EQ("Invalid coordinate.", e.what());
+        std::string message = e.what();
+        EXPECT_EQ("Invalid coordinate.", message);
     }
 }
 
@@ -228,16 +254,17 @@ TEST_F(GameTests, TestModifyTileTooLargeY) {
         game.modifyTile(2, 5, 'O');
     }
     catch (std::invalid_argument& e) {
-        EXPECT_EQ("Invalid coordinate.", e.what());
+        std::string message = e.what();
+        EXPECT_EQ("Invalid coordinate.", message);
     }
 }
 
 TEST_F(GameTests, TestModifyTileInvalidChar) {
     try {
-        game.modifyTile(2, 5, 'J');
+        game.modifyTile(2, 1, 'J');
     }
     catch (std::invalid_argument& e) {
-        EXPECT_EQ("Invalid character.", e.what());
+        std::string message = e.what();
+        EXPECT_EQ("Invalid character.", message);
     }
 }
-*/
